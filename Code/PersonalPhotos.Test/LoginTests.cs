549e6105edf045df86b63f477ac67f1626e1572c
anki0110 <ankitaguha0110@gmail.com>
2025-10-06 14:16:04 +0530

    TestCase to check Invalid model
    Test Case to check Invalid model

--- Code/PersonalPhotos.Test/LoginTests.cs
+++ Code/PersonalPhotos.Test/LoginTests.cs
@@ -1,7 +1,9 @@
 using Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Moq;
+using Newtonsoft.Json.Linq;
 using PersonalPhotos.Controllers;
+using PersonalPhotos.Models;
 
 namespace PersonalPhotos.Test
 {
@@ -9,14 +11,22 @@ namespace PersonalPhotos.Test
     {
         private readonly Mock<ILogins> _logins; // mock for Ilogin interface
         private readonly Mock<IHttpContextAccessor> _httpContextAccessor; // mock for HttpContextAccessor
-        private readonly Mock<LoginsController> _loginsController; // mock for LoginsController
+        private readonly LoginsController _loginsController; // mock for LoginsController
         public LoginTests()
         {
             _logins = new Mock<ILogins>(); // initialize mock for Ilogin interface
             _httpContextAccessor = new Mock<IHttpContextAccessor>();
-            _loginsController = new Mock<LoginsController>(_logins.Object, _httpContextAccessor.Object); // initialize mock for LoginsController
+            _loginsController = new LoginsController(_logins.Object, _httpContextAccessor.Object); // initialize mock for LoginsController
         }
 
         //ILogins Logins = Mock.Of<ILogins>(); // get mock object for Ilogin interface directly. 
+
+        [Fact]
+        public async Task Login_GivenModelStateIsInvalid_ReturnsLoginsView()
+        {
+            _loginsController.ModelState.AddModelError("Test", "Test");
+            var model = Mock.Of<LoginViewModel>();
+            var result = await _loginsController.Login(model, CancellationToken.None);
+        }
     }
 }
